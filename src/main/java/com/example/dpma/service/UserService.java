e7831384312693db4c9ac72a26ad8093a73cf7f5
tzall <cs04502@uoi.gr>
2023-05-06 21:07:25 +0300

    changes in html

--- src/main/java/com/example/dpma/service/UserService.java
+++ src/main/java/com/example/dpma/service/UserService.java
@@ -5,7 +5,7 @@ import com.example.dpma.model.User;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface UserService {
+public interface    UserService {
     public void saveUser(User user);
 
     public boolean isUserPresent(User user);
